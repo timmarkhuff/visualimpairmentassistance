2b189b1b7d1f4e5a605e7153e80fc9c769838a20
timmarkhuff <89954856+timmarkhuff@users.noreply.github.com>
2021-12-23 13:34:07 -0800

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,14 +1,15 @@
+# from https://www.youtube.com/watch?v=cAU5qMCw9Bo
 import pyrebase
 
 config = {
-  apiKey: "AIzaSyA3l-OyTRWphW2L-h3FoByqHEdu3nbxml8",
-  authDomain: "visual-impairment-assistance.firebaseapp.com",
-  databaseURL: "https://visual-impairment-assistance-default-rtdb.firebaseio.com",
-  projectId: "visual-impairment-assistance",
-  storageBucket: "visual-impairment-assistance.appspot.com",
-  messagingSenderId: "201527576702",
-  appId: "1:201527576702:web:41760af74d684fefa8b02f",
-  measurementId: "G-84Y9W41H22"
+  "apiKey": "AIzaSyA3l-OyTRWphW2L-h3FoByqHEdu3nbxml8",
+  "authDomain": "visual-impairment-assistance.firebaseapp.com",
+  "databaseURL": "https://visual-impairment-assistance-default-rtdb.firebaseio.com",
+  "projectId": "visual-impairment-assistance",
+  "storageBucket": "visual-impairment-assistance.appspot.com",
+  "messagingSenderId": "201527576702",
+  "appId": "1:201527576702:web:41760af74d684fefa8b02f",
+  "measurementId": "G-84Y9W41H22"
 };
 
 firebase = pyrebase.initialize_app(config)
